b6fa912263275c356c612ece6048a85d9a2b983d
Jeffrey Lee <vipjeffreylee@gmail.com>
2015-02-28 08:35:05 +0800

    update twitter name

--- public/js/ripple-price.js
+++ public/js/ripple-price.js
@@ -15,95 +15,95 @@ var ExchgRate={//对美元汇率
 
 var Markets = 
 [
-     {
-        name: 'US Dollars',
-        currency: 'USD',
+		 {
+				name: 'US Dollars',
+				currency: 'USD',
 				symbol:'$',
-        gateways: 
-        [
-           {name: 'bitstamp', address: 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''},
-           {name: 'snapswap', address: 'rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''}
-        ]
-     },
+				gateways: 
+				[
+					 {name: 'bitstamp', address: 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''},
+					 {name: 'snapswap', address: 'rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''}
+				]
+		 },
 
-     {
-        name: 'ChineseYuan',
-        currency: 'CNY',
+		 {
+				name: 'ChineseYuan',
+				currency: 'CNY',
 				symbol:'¥',
-        gateways: 
-        [
-           {name: 'ripplechina', address: 'razqQKzJRdB4UxFPWf5NEpEG3WMkmwgcXA',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''},
-           {name: 'ripplecn', address: 'rnuF96W4SZoCJmbHYBFoJZpR8eCaxNvekK',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''},
+				gateways: 
+				[
+					 {name: 'ripplechina', address: 'razqQKzJRdB4UxFPWf5NEpEG3WMkmwgcXA',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''},
+					 {name: 'ripplecn', address: 'rnuF96W4SZoCJmbHYBFoJZpR8eCaxNvekK',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''},
 					 {name: 'ripplefox', address: 'rKiCet8SdvWxPXnAgYarFUXMh1zCPz432Y',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''}
-        ]        
-     },
+				]        
+		 },
 		 {
-        name: 'KoreaWon',
-        currency: 'KRW',
+				name: 'KoreaWon',
+				currency: 'KRW',
 				symbol:'₩',
-        gateways: 
-        [
-           {name: 'PaxMonetar', address: 'rUkMKjQitpgAM5WTGk79xpjT38DEJY283d',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''}
-        ]                
-     },
+				gateways: 
+				[
+					 {name: 'PaxMonetar', address: 'rUkMKjQitpgAM5WTGk79xpjT38DEJY283d',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''}
+				]                
+		 },
 		 {
-        name: 'Euro',
-        currency: 'EUR',
+				name: 'Euro',
+				currency: 'EUR',
 				symbol:'€',
-        gateways: 
-        [
-           {name: 'SnapSwap', address: 'rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''},
-           {name: 'therock', address: 'rLEsXccBGNR3UPuPu2hUXPjziKC3qKSBun',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''}
-        ]        
-     },
+				gateways: 
+				[
+					 {name: 'SnapSwap', address: 'rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''},
+					 {name: 'therock', address: 'rLEsXccBGNR3UPuPu2hUXPjziKC3qKSBun',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''}
+				]        
+		 },
 		 {
-        name: 'JapanYen',
-        currency: 'JPY',
+				name: 'JapanYen',
+				currency: 'JPY',
 				symbol:'¥',
-        gateways: 
-        [
-           {name: 'TradeJapan', address: 'rMAz5ZnK73nyNUL4foAvaxdreczCkG3vA6',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''},
+				gateways: 
+				[
+					 {name: 'TradeJapan', address: 'rMAz5ZnK73nyNUL4foAvaxdreczCkG3vA6',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''},
 					 {name: 'tokyojpy', address: 'r94s8px6kSw1uZ1MV98dhSRTvc6VMPoPcN',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''},
 					 {name: 'ExchgTokyo', address: 'r9ZFPSb1TFdnJwbTMYHvVwFK1bQPUCVNfJ',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''}
-        ]                
-     },
-     {
-        name: 'Bitcoin',
-        currency: 'BTC',
+				]                
+		 },
+		 {
+				name: 'Bitcoin',
+				currency: 'BTC',
 				symbol:'Ƀ',
-        gateways: 
-        [
-           {name: 'bitstamp', address: 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''},
+				gateways: 
+				[
+					 {name: 'bitstamp', address: 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''},
 					 {name: 'btc2ripple', address: 'rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''}
-        ]                
-     },
+				]                
+		 },
 		 {
-        name: 'SingaporeDollar',
-        currency: 'SGD',
+				name: 'SingaporeDollar',
+				currency: 'SGD',
 				symbol:'$',
-        gateways: 
-        [
-           {name: 'RippleSingapore', address: 'r9Dr5xwkeLegBeXq6ujinjSBLQzQ1zQGjH',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''}
-        ]                
-     },
+				gateways: 
+				[
+					 {name: 'RippleSingapore', address: 'r9Dr5xwkeLegBeXq6ujinjSBLQzQ1zQGjH',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''}
+				]                
+		 },
 		 {
-        name: 'CanadaDollar',
-        currency: 'CAD',
+				name: 'CanadaDollar',
+				currency: 'CAD',
 				symbol:'$',
-        gateways: 
-        [
-           {name: 'RippleUnion', address: 'r3ADD8kXSUKHd6zTCKfnKT3zV9EZHjzp1S',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''}
-        ]                
-     }
+				gateways: 
+				[
+					 {name: 'RippleUnion', address: 'r3ADD8kXSUKHd6zTCKfnKT3zV9EZHjzp1S',price:{value:0,action:'asks',low:null,high:null},volume:{xrp:null,iou:null},url:''}
+				]                
+		 }
 /*
 		 {
-        name: 'Gold Bullion International',
-        currency: '0158415500000000C1F76FF6ECB0BAC600000000',
-        gateways: 
-        [
-           {name: 'GBI', address: 'rrh7rf1gV2pXAoqA8oYbpHd8TKv5ZQeo67',priceid:'#gbigbi'}
-        ]                
-     }
+				name: 'Gold Bullion International',
+				currency: '0158415500000000C1F76FF6ECB0BAC600000000',
+				gateways: 
+				[
+					 {name: 'GBI', address: 'rrh7rf1gV2pXAoqA8oYbpHd8TKv5ZQeo67',priceid:'#gbigbi'}
+				]                
+		 }
 */
 ];   
 setTimeout(loadExchgRate,10000);
@@ -129,29 +129,29 @@ function loadExchgRate(){
 	}
 }
 function subscribe(){
-  var book;
-   Markets.forEach(function(Market)
-   {
-      //console.log("init Market"+Market.name);
-      var gateways = Market.gateways;
-      gateways.forEach(function(gateway)
-      {
-           //console.log("init gateway:"+Gateways[gateway.address].name);
-           ['asks','bids'].forEach(function(action)
-           {
-              if(action == "asks")
-              {
-                book = remote.book('XRP', null, Market.currency, gateway.address);
-              }
-              else
-              {
-              	book = remote.book(Market.currency, gateway.address, 'XRP', null);
+	var book;
+	 Markets.forEach(function(Market)
+	 {
+			//console.log("init Market"+Market.name);
+			var gateways = Market.gateways;
+			gateways.forEach(function(gateway)
+			{
+					 //console.log("init gateway:"+Gateways[gateway.address].name);
+					 ['asks','bids'].forEach(function(action)
+					 {
+							if(action == "asks")
+							{
+								book = remote.book('XRP', null, Market.currency, gateway.address);
+							}
+							else
+							{
+								book = remote.book(Market.currency, gateway.address, 'XRP', null);
 
-              }
-              book.on("trade", function(tradeGets, tradePays){tradeListener(action, tradeGets, tradePays, Market.currency, gateway)});
-           });
-      });
-   });
+							}
+							book.on("trade", function(tradeGets, tradePays){tradeListener(action, tradeGets, tradePays, Market.currency, gateway)});
+					 });
+			});
+	 });
 	g$scope.$apply();
 }
 function resubscribe(){
@@ -168,33 +168,33 @@ setInterval(function(){if((new Date() - lastfreshdate)>180000) resubscribe();}, 
 function tradeListener(action, tradeGets, tradePays, currency, gateway)
 {
 	 lastfreshdate=new Date();
-   var price;
-   var xrpamount;
-   var coinamount;
-   var pricecolor; 
+	 var price;
+	 var xrpamount;
+	 var coinamount;
+	 var pricecolor; 
 	 var volumexrp,volumeiou;
-   // Ripple-lib bug
-   if (tradeGets.is_valid() ||  tradePays.is_valid()) 
-   {
-     if(action == "asks")
-      {
-     	   price = tradeGets.ratio_human(tradePays).to_human();
-         xrpamount = tradePays.to_human();
-         coinamount = tradeGets.to_human();
+	 // Ripple-lib bug
+	 if (tradeGets.is_valid() ||  tradePays.is_valid()) 
+	 {
+		 if(action == "asks")
+			{
+				 price = tradeGets.ratio_human(tradePays).to_human();
+				 xrpamount = tradePays.to_human();
+				 coinamount = tradeGets.to_human();
 				 volumexrp = tradePays.to_number();
 				 volumeiou = tradeGets.to_number();
 				 pricecolor="green";
-      }
-      else
-      {
+			}
+			else
+			{
 				 price = tradePays.ratio_human(tradeGets).to_human();
-         xrpamount = tradeGets.to_human();
-         coinamount = tradePays.to_human();
+				 xrpamount = tradeGets.to_human();
+				 coinamount = tradePays.to_human();
 				 volumexrp = tradeGets.to_number();
 				 volumeiou = tradePays.to_number();
 				 pricecolor="red";
-      }
-      price=price.substring(0,9);
+			}
+			price=price.substring(0,9);
 			//console.log(gateway.name+price);
 			/*var pos;
 			for(pos=price.length-1;pos>=0;pos--){ 
@@ -210,7 +210,7 @@ function tradeListener(action, tradeGets, tradePays, currency, gateway)
 			gateway.volume.xrp+=droptoxrp(volumexrp);
 			gateway.volume.iou+=volumeiou;
 			g$scope.$apply();
-  		//$(gateway.priceid).html(price);
+			//$(gateway.priceid).html(price);
 			//$(gateway.priceid).css("color",pricecolor);
 			//console.log(gateway.priceid+price);
 			var trade={};
@@ -235,7 +235,7 @@ function tradeListener(action, tradeGets, tradePays, currency, gateway)
 			}
 			$("#"+currency+gateway.address).attr("data-content",trade.time+" price:<font color='"+pricecolor+"'>"+trade.price+"</font> size:"+trade.size+"<br>"+content);	
 			
-  }
+	}
 }
 
 
